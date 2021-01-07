88c3a11122f4f1f28a0d80363a7cd91f16d29b67
Masumur Rahman <csemasum@gmail.com>
2021-01-07 21:01:23 +0600

    new push

---
+++ js/main.js
@@ -0,0 +1,209 @@
+(function ($) {
+"use strict";
+
+
+
+
+	/*----------------------------
+       jQuery Mean Menu
+       ------------------------------ */
+	$("#mobile-menu").meanmenu({
+		meanMenuContainer: ".mobile-menu",
+		meanScreenWidth: "767"
+	});
+	/*----------------------------
+   Sticky menu active
+   ------------------------------ */
+	function fixed_top_menu() {
+		var windows = $(window);
+		windows.on("scroll", function () {
+			var header_height = $(".main-navigation").height();
+			var scrollTop = windows.scrollTop();
+			if (scrollTop > header_height) {
+				$(".main-navigation").addClass("sticky");
+			} else {
+				$(".main-navigation").removeClass("sticky");
+			}
+		});
+	}
+	fixed_top_menu();
+
+
+	/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+
+$('.grid').imagesLoaded( function() {
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  masonry: {
+		// use outer width of grid-sizer for columnWidth
+		columnWidth: '.grid-item',
+	  }
+	});
+});
+
+// filter items on button click
+$('.portfolio-menu').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+
+//Testimonials
+	if ($('.testimonial-slider').length>0) {
+		$(".testimonial-slider").slick({
+			slidesToShow:1,
+			slidesToScroll:1,
+			arrows:false,
+			fade:true,
+			asNavFor:".testimonial-nav"
+		});
+
+		$(".testimonial-nav").slick({
+			slidesToShow:5,
+			slidesToScroll:1,
+			asNavFor:".testimonial-slider",
+			arrows:false,
+			centerMode:true,
+			focusOnSelect:true,
+			variableWidth:false,
+			responsive:[
+				{
+					breakpoint:991,
+					settings:{
+						slidesToShow:3,
+						arrows:false
+					}
+				},
+				{
+					breakpoint:480,
+					settings:{
+						slidesToShow:1,
+						arrows:false
+					}
+				}
+			]
+		});
+	}
+
+	//Screenshots
+	if ($('.screenshot-slider').length>0) {
+		$(".screenshot-slider").owlCarousel({
+			responsive:{
+				0:{
+					items:1
+				},
+				768:{
+					items:2
+				},
+				960 : {
+					items:4
+				}
+			},
+			responsiveClass:true,
+			autoplay:true,
+			autoplaySpeed:1000,
+			margin:30,
+			dotsEach:2
+		});
+	}
+
+
+
+//for menu active class
+$('.portfolio-menu button').on('click', function(event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+// Brand Logo Sliding
+
+	$('.brand-logo-slider').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		slidesToShow: 7,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+
+		]
+	});
+
+	// Product Sliding
+
+	$('.product-slider').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+
+		]
+	});
+
+})(jQuery);	
